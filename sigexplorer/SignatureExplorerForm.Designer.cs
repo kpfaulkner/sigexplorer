e3d10da78e7a34da43086fa00d959bd5cd48307e
Ken Faulkner <ken.faulkner@gmail.com>
2012-06-18 20:58:19 +1000

    added file sizes, common and shared to display

--- sigexplorer/SignatureExplorerForm.Designer.cs
+++ sigexplorer/SignatureExplorerForm.Designer.cs
@@ -28,29 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.loadSigButton = new System.Windows.Forms.Button();
             this.sigTreeView = new System.Windows.Forms.TreeView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.OffsetSortButton = new System.Windows.Forms.RadioButton();
             this.SizeSortButton = new System.Windows.Forms.RadioButton();
+            this.sigTreeView2 = new System.Windows.Forms.TreeView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.file2TotalSize = new System.Windows.Forms.Label();
+            this.file1TotalSize = new System.Windows.Forms.Label();
+            this.file2Label = new System.Windows.Forms.Label();
+            this.file1Label = new System.Windows.Forms.Label();
+            this.sharedSize = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.newSize = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // loadSigButton
-            // 
-            this.loadSigButton.Location = new System.Drawing.Point(12, 12);
-            this.loadSigButton.Name = "loadSigButton";
-            this.loadSigButton.Size = new System.Drawing.Size(75, 23);
-            this.loadSigButton.TabIndex = 0;
-            this.loadSigButton.Text = "Load Sig";
-            this.loadSigButton.UseVisualStyleBackColor = true;
-            this.loadSigButton.Click += new System.EventHandler(this.loadSigButton_Click);
             // 
             // sigTreeView
             // 
-            this.sigTreeView.Location = new System.Drawing.Point(12, 120);
+            this.sigTreeView.Location = new System.Drawing.Point(14, 102);
             this.sigTreeView.Name = "sigTreeView";
-            this.sigTreeView.Size = new System.Drawing.Size(289, 542);
+            this.sigTreeView.Size = new System.Drawing.Size(267, 474);
             this.sigTreeView.TabIndex = 1;
             this.sigTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.sigTreeView_AfterSelect);
             // 
@@ -58,7 +60,7 @@
             // 
             this.groupBox1.Controls.Add(this.OffsetSortButton);
             this.groupBox1.Controls.Add(this.SizeSortButton);
-            this.groupBox1.Location = new System.Drawing.Point(12, 54);
+            this.groupBox1.Location = new System.Drawing.Point(7, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(175, 60);
             this.groupBox1.TabIndex = 3;
@@ -88,30 +90,165 @@
             this.SizeSortButton.UseVisualStyleBackColor = true;
             this.SizeSortButton.CheckedChanged += new System.EventHandler(this.SizeSortButton_CheckedChanged);
             // 
+            // sigTreeView2
+            // 
+            this.sigTreeView2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sigTreeView2.Location = new System.Drawing.Point(298, 102);
+            this.sigTreeView2.Name = "sigTreeView2";
+            this.sigTreeView2.Size = new System.Drawing.Size(293, 474);
+            this.sigTreeView2.TabIndex = 4;
+            this.sigTreeView2.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.sigTreeView2_AfterSelect);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.newSize);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.sharedSize);
+            this.groupBox2.Controls.Add(this.file2TotalSize);
+            this.groupBox2.Controls.Add(this.file1TotalSize);
+            this.groupBox2.Controls.Add(this.file2Label);
+            this.groupBox2.Controls.Add(this.file1Label);
+            this.groupBox2.Controls.Add(this.sigTreeView2);
+            this.groupBox2.Controls.Add(this.sigTreeView);
+            this.groupBox2.Location = new System.Drawing.Point(15, 83);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(605, 595);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // file2TotalSize
+            // 
+            this.file2TotalSize.AutoSize = true;
+            this.file2TotalSize.Location = new System.Drawing.Point(328, 66);
+            this.file2TotalSize.Name = "file2TotalSize";
+            this.file2TotalSize.Size = new System.Drawing.Size(0, 13);
+            this.file2TotalSize.TabIndex = 8;
+            // 
+            // file1TotalSize
+            // 
+            this.file1TotalSize.AutoSize = true;
+            this.file1TotalSize.Location = new System.Drawing.Point(47, 67);
+            this.file1TotalSize.Name = "file1TotalSize";
+            this.file1TotalSize.Size = new System.Drawing.Size(0, 13);
+            this.file1TotalSize.TabIndex = 7;
+            this.file1TotalSize.Click += new System.EventHandler(this.file1TotalSize_Click);
+            // 
+            // file2Label
+            // 
+            this.file2Label.AutoSize = true;
+            this.file2Label.Location = new System.Drawing.Point(295, 16);
+            this.file2Label.Name = "file2Label";
+            this.file2Label.Size = new System.Drawing.Size(0, 13);
+            this.file2Label.TabIndex = 6;
+            // 
+            // file1Label
+            // 
+            this.file1Label.AutoSize = true;
+            this.file1Label.Location = new System.Drawing.Point(11, 15);
+            this.file1Label.Name = "file1Label";
+            this.file1Label.Size = new System.Drawing.Size(0, 13);
+            this.file1Label.TabIndex = 5;
+            this.file1Label.Click += new System.EventHandler(this.file1Label_Click);
+            // 
+            // sharedSize
+            // 
+            this.sharedSize.AutoSize = true;
+            this.sharedSize.Location = new System.Drawing.Point(186, 67);
+            this.sharedSize.Name = "sharedSize";
+            this.sharedSize.Size = new System.Drawing.Size(0, 13);
+            this.sharedSize.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Size";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Size";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(144, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Shared";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(405, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "New";
+            // 
+            // newSize
+            // 
+            this.newSize.AutoSize = true;
+            this.newSize.Location = new System.Drawing.Point(453, 66);
+            this.newSize.Name = "newSize";
+            this.newSize.Size = new System.Drawing.Size(0, 13);
+            this.newSize.TabIndex = 14;
+            // 
             // SignatureExplorerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(317, 641);
+            this.ClientSize = new System.Drawing.Size(632, 780);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.sigTreeView);
-            this.Controls.Add(this.loadSigButton);
             this.Name = "SignatureExplorerForm";
             this.Text = "Signature Explorer";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button loadSigButton;
         private System.Windows.Forms.TreeView sigTreeView;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton SizeSortButton;
         private System.Windows.Forms.RadioButton OffsetSortButton;
+        private System.Windows.Forms.TreeView sigTreeView2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label file1Label;
+        private System.Windows.Forms.Label file2Label;
+        private System.Windows.Forms.Label file1TotalSize;
+        private System.Windows.Forms.Label file2TotalSize;
+        private System.Windows.Forms.Label sharedSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label newSize;
+        private System.Windows.Forms.Label label4;
 
     }
 }
--- sigexplorer/SignatureExplorerForm.cs
+++ sigexplorer/SignatureExplorerForm.cs
@@ -18,24 +18,42 @@ namespace sigexplorer
     public partial class SignatureExplorerForm : Form
     {
 
-        SizeBasedCompleteSignature sig;
+        SizeBasedCompleteSignature sig1;
+        SizeBasedCompleteSignature sig2;
+
+        int sig1Total;
+        int sig2Total;
+        string sig1Filename;
+        string sig2Filename;
+
+        uint file1Size;
+        uint file2Size;
+        uint bothFilesShared;
 
         public SignatureExplorerForm()
         {
             InitializeComponent();
             sigTreeView.AllowDrop = true;
+            sigTreeView2.AllowDrop = true;
+            sigTreeView.ShowNodeToolTips = true;
+            sigTreeView2.ShowNodeToolTips = true;
             this.AllowDrop = true;
             sigTreeView.DoubleClick += new EventHandler(SigTreeViewClick);
             sigTreeView.DragDrop += new DragEventHandler(SignatureExplorerFormDragDrop);
             sigTreeView.DragEnter += new DragEventHandler(SignatureExplorerFormDragEnter);
+            
+            sigTreeView2.DoubleClick += new EventHandler(SigTreeViewClick2);
+            sigTreeView2.DragDrop += new DragEventHandler(SignatureExplorerFormDragDrop2);
+            sigTreeView2.DragEnter += new DragEventHandler(SignatureExplorerFormDragEnter);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.AllowDrop = true;
-            this.DragDrop += new DragEventHandler(SignatureExplorerFormDragDrop);
-            this.DragEnter += new DragEventHandler(SignatureExplorerFormDragEnter);
+            //this.AllowDrop = true;
+            //this.DragDrop += new DragEventHandler(SignatureExplorerFormDragDrop);
+            //this.DragEnter += new DragEventHandler(SignatureExplorerFormDragEnter);
         }
+
 
         void SignatureExplorerFormDragDrop(object sender, DragEventArgs e)
         {
@@ -44,8 +62,21 @@ namespace sigexplorer
 
             if (filePaths.Length > 0)
             {
+                file1Label.Text = filePaths[0];
+                LoadSigFile(filePaths[0], ref sig1);
+            }
 
-                LoadSigFile(filePaths[0]);
+        }
+
+        void SignatureExplorerFormDragDrop2(object sender, DragEventArgs e)
+        {
+
+            string[] filePaths = (string[])(e.Data.GetData(DataFormats.FileDrop));
+
+            if (filePaths.Length > 0)
+            {
+                file2Label.Text = filePaths[0];
+                LoadSigFile(filePaths[0], ref sig2);
             }
 
         }
@@ -65,111 +96,286 @@ namespace sigexplorer
         {
 
             var selectedNode = sigTreeView.SelectedNode;
+            ProcessDoubleClick(selectedNode, sig1);
+        }
+
+        private void SigTreeViewClick2(object sender, EventArgs e)
+        {
+
+            var selectedNode = sigTreeView2.SelectedNode;
+            ProcessDoubleClick(selectedNode, sig2);
+        }
+
+        private void ProcessDoubleClick(TreeNode selectedNode, SizeBasedCompleteSignature sig)
+        {
 
             if (selectedNode != null && selectedNode.Parent != null)
             {
                 var sp = selectedNode.Parent.Text.Split();
                 var sigSize = Convert.ToInt32(sp[0]);
-                var offset = Convert.ToUInt32(selectedNode.Text);
+                var offset = Convert.ToUInt32(selectedNode.Text.Split()[0]);
 
                 var specificSig = (from s in sig.Signatures[sigSize].SignatureList where s.Offset == offset select s).First<BlockSignature>();
 
-                var sb = new StringBuilder();
-                foreach (var b in specificSig.MD5Signature)
-                {
-                    sb.AppendFormat("{0:X2}", b);
-                }
+                var md5String = ByteArrayToString(specificSig.MD5Signature);
+               
                 var msg = string.Format("Offset: {0}\nSize: {1}\nRollingSig: {2}\nMD5: {3}", specificSig.Offset.ToString(),
-                                        specificSig.Size.ToString(), specificSig.RollingSignature.ToString(), sb.ToString());
+                                        specificSig.Size.ToString(), specificSig.RollingSignature.ToString(), md5String);
 
                 var dialog = MessageBox.Show(msg);
             }
         }
 
-        private void PopulateSignatureTreeBySize( )
+        
+        private List< Tuple<byte[], uint>> GenerateMD5ListFromSig(SizeBasedCompleteSignature? sig)
         {
-            sigTreeView.Nodes.Clear();
+            
+            var md5List = new List< Tuple<byte[], uint>>();
 
-            foreach (var size in sig.Signatures.Keys)
+            if (sig.HasValue && sig.Value.Signatures != null)
             {
-                var sizeMsg = string.Format("{0} ({1})", size, sig.Signatures[size].SignatureList.Count());
-
-                var node = sigTreeView.Nodes.Add(sizeMsg);
-                foreach (var sSig in sig.Signatures[size].SignatureList)
+                foreach (var size in sig.Value.Signatures.Keys)
                 {
-                    node.Nodes.Add(sSig.Offset.ToString());
+                    foreach (var sSig in sig.Value.Signatures[size].SignatureList)
+                    {
+                        var tuple = new Tuple<byte[], uint>(sSig.MD5Signature, sSig.Offset);
+                        md5List.Add(tuple);
+                    }
+                }
+            }
+            return md5List;
+        }
+
+        private void PopulateSignatureTreeBySize(TreeView treeView, SizeBasedCompleteSignature sig, SizeBasedCompleteSignature? otherSig)
+        {
+
+            var sigList = new List<BlockSignature>();
+            if (sig.Signatures != null)
+            {
+                foreach (var size in sig.Signatures.Keys)
+                {
+                    foreach (var sSig in sig.Signatures[size].SignatureList)
+                    {
+                        sigList.Add(sSig);
+                    }
                 }
 
+                var sortedSigList = (from s in sigList orderby s.Size select s).ToList<BlockSignature>();
+
+                sortedSigList = sortedSigList.Reverse<BlockSignature>().ToList<BlockSignature>();
+
+                var otherSigMD5List = GenerateMD5ListFromSig(otherSig);
+                PopulateSignatureTreeFromList(treeView, sortedSigList, otherSigMD5List);
             }
         }
 
-        private void PopulateSignatureTreeByOffset()
+        private void PopulateSignatureTreeByOffset(TreeView treeView, SizeBasedCompleteSignature sig, SizeBasedCompleteSignature? otherSig)
         {
-            sigTreeView.Nodes.Clear();
-
             var sigList = new List<BlockSignature>();
 
-            foreach (var size in sig.Signatures.Keys)
+            if (sig.Signatures != null)
             {
-                foreach (var sSig in sig.Signatures[size].SignatureList)
+                foreach (var size in sig.Signatures.Keys)
                 {
-                    sigList.Add(sSig);
+                    foreach (var sSig in sig.Signatures[size].SignatureList)
+                    {
+                        sigList.Add(sSig);
+                    }
+                }
+
+                var sortedSigList = (from s in sigList orderby s.Offset select s).ToList<BlockSignature>();
+
+                var otherSigMD5List = GenerateMD5ListFromSig(otherSig);
+
+                PopulateSignatureTreeFromList(treeView, sortedSigList, otherSigMD5List);
+            }
+
+        }
+
+        private bool DoesMD5ExistInList(byte[] md5, List<Tuple<byte[], uint>> md5List)
+        {
+            var res = false;
+            foreach (var m in md5List)
+            {
+                if (m.Item1.SequenceEqual(md5))
+                {
+                    res = true;
+                    break;
                 }
             }
 
-            var sortedSigList = from s in sigList orderby s.Offset select s;
+            return res;
+        }
 
+
+        private void PopulateSignatureTreeFromList(TreeView treeView, List<BlockSignature> sortedSigList, List<Tuple<byte[], uint>> otherSigMD5List)
+        {
+            treeView.Nodes.Clear();
             uint lastSize = 0;
             TreeNode currentNode = null;
+
+            var md5Dict = GenerateMD5Dict(otherSigMD5List);
+            int size = 0;
             foreach (var s in sortedSigList)
             {
                 // if new size, then add new root node.
                 if (s.Size != lastSize)
                 {
-                    
+
                     if (currentNode != null)
                     {
                         currentNode.Text = string.Format("{0} ({1})", lastSize, currentNode.Nodes.Count);
                     }
 
-                    currentNode = sigTreeView.Nodes.Add(s.Size.ToString());
+                    currentNode = treeView.Nodes.Add(s.Size.ToString());
                     lastSize = s.Size;
                 }
-                
-                currentNode.Nodes.Add(s.Offset.ToString());
+
+                TreeNode smallNode;
+                if (otherSigMD5List != null && otherSigMD5List.Count > 0)
+                {
+                    file2Size += s.Size;
+                   
+                    var md5Str = ByteArrayToString(s.MD5Signature);
+                    if (md5Dict.ContainsKey(md5Str))
+                    {
+                        smallNode = currentNode.Nodes.Add(s.Offset.ToString() + " : " + md5Dict[md5Str].ToString() );
+                        bothFilesShared += s.Size;
+
+                        smallNode.ForeColor = Color.Green;
+                        //smallNode.Text = "foo";
+                        // smallNode.Text + " : " + "foo";
+                        //md5Dict[md5Str].ToString();
+                    }
+                    else
+                    {
+                        smallNode = currentNode.Nodes.Add(s.Offset.ToString());
+                    }
+                }
+                else
+                {
+                    // othersig == null means this is first tree. So just add to file1Size
+                    file1Size += s.Size;
+
+                    smallNode = currentNode.Nodes.Add(s.Offset.ToString());
+                }
+
+                var md5String = ByteArrayToString(s.MD5Signature);
+                var msg = string.Format("Offset: {0}\nSize: {1}\nRollingSig: {2}\nMD5: {3}", s.Offset.ToString(),
+                                        s.Size.ToString(), s.RollingSignature.ToString(), md5String);
+
+
+                smallNode.ToolTipText = msg;
+               
                 
             }
+            currentNode.Text = string.Format("{0} ({1})", lastSize, currentNode.Nodes.Count);
+            
+        }
 
+        private string ByteArrayToString(byte[] byteArray)
+        {
+            var sb = new StringBuilder();
+            sb.Clear();
+            foreach (var b in byteArray)
+            {
+                sb.AppendFormat("{0:X2}", b);
+            }
 
+            return sb.ToString();
+        }
+
+        private Dictionary<string, uint> GenerateMD5Dict(List<Tuple<byte[], uint>> otherSigMD5List)
+        {
+            var res = new Dictionary<string, uint>();
+            var sb = new StringBuilder();
+            foreach (var i in otherSigMD5List)
+            {
+                var md5String = ByteArrayToString(i.Item1);
+                res[md5String] = i.Item2;
+            }
+            return res;
         }
 
         private void PopulateSignatureTree( bool performOffsetSort = false )
         {
             sigTreeView.Nodes.Clear();
+            sigTreeView2.Nodes.Clear();
+
+            file1Size = 0;
+            file2Size = 0;
+            bothFilesShared = 0;
 
             if (!performOffsetSort)
             {
-                PopulateSignatureTreeBySize();
+                PopulateSignatureTreeBySize( sigTreeView, sig1, null);
+                PopulateSignatureTreeBySize( sigTreeView2, sig2, sig1);
             }
             else
             {
-                PopulateSignatureTreeByOffset();
+                PopulateSignatureTreeByOffset(sigTreeView, sig1, null);
+                PopulateSignatureTreeByOffset( sigTreeView2, sig2, sig1);
             }
 
+            file1TotalSize.Text = file1Size.ToString();
+            file2TotalSize.Text = file2Size.ToString();
+            sharedSize.Text = bothFilesShared.ToString();
+            newSize.Text = (file2Size - bothFilesShared).ToString();
             sigTreeView.Update();
+            sigTreeView2.Update();
+        }
+
+
+        /// <summary>
+        /// Quick check to make sure we dont have any dupe md5's
+        /// </summary>
+        /// <param name="sig"></param>
+        /// <returns></returns>
+        private Boolean VerifySignature(SizeBasedCompleteSignature sig)
+        {
+            var myDict = new Dictionary<string, int>();
+            var valid = true;
+            var count = 0;
+            foreach (var size in sig.Signatures.Keys)
+            {
+                foreach (var sSig in sig.Signatures[size].SignatureList)
+                {
+                    var md5 = sSig.MD5Signature;
+                    var md5Str = ByteArrayToString(md5);
+                    if (myDict.ContainsKey( md5Str) )
+                    {
+                        valid = false;
+                        count++;
+                    }
+                    else
+                    {
+                        myDict[md5Str] = 1;
+                    }
+
+
+
+                }
+            }
+
+            return valid;
         }
 
         /// <summary>
         /// Loads the sig file.
         /// </summary>
         /// <param name="filename"></param>
-        private void LoadSigFile(string filename)
+        private void LoadSigFile(string filename, ref SizeBasedCompleteSignature sig)
         {
             using (var fs = new FileStream(filename, FileMode.Open))
             {
                 sig = SerializationHelper.ReadSizeBasedBinarySignature( fs );
+
+                VerifySignature(sig);
             }
 
+            file1Size = 0;
+            file2Size = 0;
+            bothFilesShared = 0;
             PopulateSignatureTree();
 
         }
@@ -185,7 +391,7 @@ namespace sigexplorer
             dialog.ShowDialog();
             var fn = dialog.FileName;
 
-            LoadSigFile(fn);
+            //LoadSigFile(fn);
 
             
         }
@@ -203,6 +409,31 @@ namespace sigexplorer
         private void SizeSortButton_CheckedChanged(object sender, EventArgs e)
         {
             PopulateSignatureTree(false);
+        }
+
+        private void sigTreeView2_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void file1TotalSize_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void file1Label_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
